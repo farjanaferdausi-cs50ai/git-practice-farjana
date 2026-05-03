39a9dd3e2b9fca438c8ac4fa8379eedfeca2c76e
Farjana Ferdausi <farjana.rafi1983@gmail.com>
2026-05-03 14:33:44 +0600

    Add error handling to analysis

--- src/utils.py
+++ src/utils.py
@@ -3,3 +3,9 @@ def total_passengers(data):
 
 def survival_count(data):
     return sum(p["Survived"] for p in data)
+
+def safe_survival_count(data):
+    try:
+        return sum(p["Survived"] for p in data)
+    except:
+        return 0
